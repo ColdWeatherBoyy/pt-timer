dde0faaf870ee3c79492a4d9d30e0cb0483933f3
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-04-27 14:13:09 -0400

    initial set up of main page with where timers will be

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+	return (
+		<main className="flex min-h-screen flex-col items-center gap-y-8 p-24">
+			<div className="text-4xl text-center">Uh... there's nothing here...</div>
+			<div className="text-2xl text-center">So that's gonna be a...</div>
+			<div className="text-8xl text-center">404</div>
+		</main>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+	const [timers, setTimers] = useState([5, 10, 15]);
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>Hey</div>
+			{timers.map((timers) => (
+				<div className="flex w-3/4 justify-content bg-zinc-200 rounded-lg shadow-md p-10">
+					{timers}
+				</div>
+			))}
 		</main>
 	);
 }
