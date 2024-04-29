1f796db70cdfb95134b1a2701de948ca56d696a8
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-04-28 23:55:06 -0400

    missed one thing

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -19,7 +19,7 @@ const Button: React.FC<ButtonProps> = ({
 	return (
 		<button
 			onClick={onClick}
-			className={`w-fit p-3 rounded-lg shadow-lg hover:shadow-xl ${buttonColor}  ${
+			className={`w-fit p-2 rounded-lg shadow-lg hover:shadow-xl ${buttonColor}  ${
 				animate
 					? "hover:animate-trembleHover active:animate-trembleActive"
 					: "hover:scale-105 hover:-translate-y-0.5 hover:translate-x-0.5 active:translate-y-0 active:translate-x-0 active:scale-95"
